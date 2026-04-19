ed275578df3bd97df7e387d9d69ec8a241ccfcc5
tomasdigiu <sebasdigiuseppeybarrios@frba.utn.edu.ar>
2026-04-19 15:06:05 -0300

    Creados atributos y metodos para Producto y PrecioProducto

--- src/main/java/com/carrito/catalogo/PrecioProducto.java
+++ src/main/java/com/carrito/catalogo/PrecioProducto.java
@@ -1,4 +1,19 @@
 package com.carrito.catalogo;
 
+import java.util.Date;
+
 public class PrecioProducto {
+    private Date fechaInicioVigencia;
+    private Date fechaFinVigencia;
+    private int precio;
+
+
+    public Date getFechaInicioVigencia(){return fechaInicioVigencia;}
+    public Date getFechaFinVigencia(){return fechaFinVigencia;}
+
+    public boolean getCumpleVigencia(Date fecha){
+        return fecha.after(fechaInicioVigencia) && fecha.before(fechaFinVigencia);
+    }
+
+    public int getPrecio() {return precio;}
 }
--- src/main/java/com/carrito/catalogo/Producto.java
+++ src/main/java/com/carrito/catalogo/Producto.java
@@ -1,4 +1,25 @@
 package com.carrito.catalogo;
 
+import java.util.List;
+import java.util.Date;
+import java.util.NoSuchElementException;
+
+
 public class Producto {
+    private String ean13;
+    private String nombre;
+    private List<PrecioProducto> preciosHistoricos;
+
+    public String getean13() {return ean13;}
+    public String getNombre() {return nombre;}
+    // public String getNombreCorto()
+    public int getPrecio(Date fecha){
+        for (PrecioProducto precioProducto : preciosHistoricos) {
+            if (precioProducto.getCumpleVigencia(fecha)){
+                return precioProducto.getPrecio();
+            }
+        }
+        throw new NoSuchElementException("No existe precio para la fecha " + fecha);
+    }
+
 }
